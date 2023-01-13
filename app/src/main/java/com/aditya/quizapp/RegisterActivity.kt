c22a374de642efd8c3c481c71e0c8c8cb102adee
Aditya Raj Singh <adityarajsingh0190@gmail.com>
2023-01-13 15:05:49 +0530

    Initial Commit

--- app/src/main/java/com/aditya/quizapp/RegisterActivity.kt
+++ app/src/main/java/com/aditya/quizapp/RegisterActivity.kt
@@ -7,5 +7,9 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+
+
+
     }
 }
