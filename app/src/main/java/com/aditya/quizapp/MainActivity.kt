f739963a6f4403943cc7daf414e96ef45c16dbbf
Aditya Raj Singh <adityarajsingh0190@gmail.com>
2023-02-17 12:29:36 +0530

    back btn bug fixed

--- app/src/main/java/com/aditya/quizapp/MainActivity.kt
+++ app/src/main/java/com/aditya/quizapp/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.aditya.quizapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.aditya.quizapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
-   private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding =ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
-
 }
--- app/src/main/java/com/aditya/quizapp/fragments/ScoreBoard.kt
+++ app/src/main/java/com/aditya/quizapp/fragments/ScoreBoard.kt
@@ -4,7 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.fragment.findNavController
+import com.aditya.quizapp.R
 import com.aditya.quizapp.databinding.FragmentScoreBoardBinding
 
 class ScoreBoard : Fragment() {
@@ -15,11 +21,24 @@ class ScoreBoard : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentScoreBoardBinding.inflate(layoutInflater)
-
         binding.tbFinishedQuiz.tvToolbarTitle.text = "Score Board"
         val score = arguments?.getString("YourScore").toString()
         binding.scoreStudent.text = score
+
+        var callback: OnBackPressedCallback =
+            object : OnBackPressedCallback(true /* enabled by default */) {
+                override fun handleOnBackPressed() {
+                    // Handle the back button event
+                    findNavController().navigate(R.id.action_scoreBoard_to_studentDashboard)
+                }
+            }
+        requireActivity().onBackPressedDispatcher.addCallback(requireActivity(), callback);
         return binding.root
     }
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tbFinishedQuiz.ivBackBtnToolbar.setOnClickListener {
+            findNavController().navigate(R.id.action_scoreBoard_to_studentDashboard)
+        }
+    }
 }
